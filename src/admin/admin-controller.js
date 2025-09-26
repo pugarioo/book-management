75b52b1471a173a0cff5418c7d0e9057a2ac46dc
Joshua Lopez <lopezjoshuapanaligan@gmail.com>
2025-09-26 18:55:53 +0800

    added handlers for actions except update

--- src/admin/admin-controller.js
+++ src/admin/admin-controller.js
@@ -37,3 +37,104 @@ function showEdit(btn) {
 
     editContainer.classList.add("open")
 }
+
+
+function addBook() {
+    const form = document.getElementById("add-form")
+    const formData = new FormData(form)
+
+    fetch("api/add_book.php", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+        // Logic for notification
+        if (data.status === "success") {
+            // TODO
+        }
+    })
+    .catch(err => console.error("Error: ", err))
+}
+
+function getBooks() {
+    fetch("api/browse.php" , {
+        method: "GET"
+    })
+    .then(res => res.json())
+    .then(data => renderBooks(data))
+    .catch(err => console.error("Error: ", err))
+}
+
+function searchBook() {
+    const keyword = (document.getElementById("admin-search-bar").value).trim()
+
+
+    fetch("api/search_book.php?keyword=" + encodeURIComponent(keyword), {
+        method: "GET"
+    })
+    .then(res => res.json())
+    .then(data => renderBooks(data))
+    .catch(err => console.error("Error: ", err))
+}
+
+function renderBooks(data) {
+    const table = document.querySelector("#book-table tbody")
+    table.innerHTML = "";
+
+    data.forEach(row => {
+        const tableRow = document.createElement("tr")
+
+        const editBtn = document.createElement("button")
+        editBtn.className = "edit-del-btn"
+        editBtn.setAttribute("onclick", "showEdit(this)")
+        editBtn.textContent = "Edit";
+
+        const delBtn = document.createElement("button")
+        delBtn.className = "edit-del-btn"
+        delBtn.setAttribute("onclick", "")
+        delBtn.textContent = "Delete";
+
+        tableRow.appendChild(createCell(row.id))
+        tableRow.appendChild(createCell(row.title))
+        tableRow.appendChild(createCell(row.author))
+        tableRow.appendChild(createCell(row.year))
+        tableRow.appendChild(createCell(row.category))
+        tableRow.appendChild(createCell(row.status))
+        tableRow.appendChild(editBtn)
+        tableRow.appendChild(delBtn)
+
+        table.appendChild(tableRow)
+        })
+}
+
+function createCell(value) {
+    const cell = document.createElement("td")
+    cell.textContent = value
+
+    return cell
+}
+
+function deleteBook(btn) {
+    const row = btn.closest("tr")
+    const cells = row.querySelectorAll("td")
+    const id = cells[0].textContent
+    
+    fetch("api/delete.php", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `id=${id}`
+    })
+    .then(res => res.json())
+    .then(data => {
+        // Logic to notify deletion
+        if (data.status === "success") {
+            row.remove
+        }
+        else {
+            
+        }
+    })
+    .catch(err => console.error("Error: ", err))
+}
